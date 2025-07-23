6431f1b85674f30b0790b3f1bb66cafdb5abbc94
Divyanshu <dgdivyanshu69@gmail.com>
2025-07-23 23:29:05 +0530

    Connected frontend to backend API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
     if (!password) return;
     setLoading(true);
     try {
-      const response = await fetch("http://127.0.0.1:5000/predict", {
+      const response = await fetch("https://password-analyzer-api-r397.onrender.com/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ password, personal_info: personalInfo }),
